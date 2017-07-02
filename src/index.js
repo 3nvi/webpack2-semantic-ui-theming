3eab50d177ae71b7a373ace09292579c54300200
3nvi <agg.arvanitaks@gmail.com>
2017-07-02 21:42:57 +0300

    showcased individual component bundling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+// this would bundle all Semantic components
 require("semantic-ui-less/semantic.less");
+
+// this would bundle only our selected components through our local semantic.less file
+// require("../my-semantic-theme/semantic.less");
+
